db774cd3ad0ab7b49dc2acdeecf3d8448a064a54
Matthew O'Brien <maobrien11235@gmail.com>
2017-09-04 05:00:27 -0500

    Started program. Building out queries.

--- python_code/main.py
+++ python_code/main.py
@@ -7,5 +7,25 @@ Created on Mon Sep  4 04:31:02 2017
 Purpose: To dig into the 900K observation Lending Club dataset. 
          The project will also be used as a test project for John Haley's
          distributed processing environment. 
+         
+Notes: Data stored in:
+        C:\Users\matthew\Documents\lending-club-loan-data
 """
 
+#--------------------------------------------------------------------------#
+
+import sqlite3
+import pandas as pd
+
+
+conn = sqlite3.connect(r'C:\Users\matthew\Documents\lending-club-loan-data\database.sqlite')
+
+cur = conn.cursor()
+
+cur.execute("""
+            SELECT *
+            from loan
+            LIMIT 100
+            """)
+pd.DataFrame(cur.fetchall())
+print
