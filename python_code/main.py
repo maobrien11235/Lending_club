de01dcce5c43e9706631c5290092ebbab4a9da32
Matthew O'Brien <maobrien11235@gmail.com>
2017-09-04 04:44:09 -0500

    Downloaded data and started main.py file.

---
+++ python_code/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  4 04:31:02 2017
+
+@author: Matthew A. O'Brien
+
+Purpose: To dig into the 900K observation Lending Club dataset. 
+         The project will also be used as a test project for John Haley's
+         distributed processing environment. 
+"""
+
